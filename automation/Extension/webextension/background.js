5120e9cef9dadc2f1a2130f03f656d003b169744
Cavan Hussein <cavanhussein@comcast.net>
2016-11-17 18:01:18 -0600

    initial javascript browser functions

--- automation/Extension/webextension/background.js
+++ automation/Extension/webextension/background.js
@@ -6,3 +6,38 @@ chrome.app.runtime.onLaunched.addListener(function() {
     }
   });
 });
+
+function browser_scroll(x-coord, y-coord) {
+    window.scrollTo(x-coord, y-coord)
+}
+
+function listCookies() {
+    return document.cookie.split(';');
+}
+
+function close_window() {
+    window.close();
+}
+
+function open_new_window(url) {
+    window.open(url);
+}
+
+function tab_restart_browser() {
+    var current_url = window.location.href
+    close_window()
+    open_new_window(current_url)
+}
+
+function get_links_in_current_window() {
+    var link_list = [];
+    var links = document.links;
+    for(var i = 0; i < links.length; i++) {
+        link_list.push(links[i].href)
+    }
+    return link_list
+}
+
+function dump_page_source(url) {
+    return document.documentElement.outerHTML
+}
