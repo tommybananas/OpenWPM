2fb0a7782c94e55c50202f18a0d4c38d581396f7
Sai Sunkam <sunk0015@umn.edu>
2016-12-04 14:01:21 -0600

    Added background.js

--- automation/Extension/webextension/background.js
+++ automation/Extension/webextension/background.js
@@ -56,3 +56,18 @@ function reportCookies(obj){
   console.log('reportcookies', obj);
   return true;
 }
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+    console.log(response.farewell);
+  });
+});
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+  });
