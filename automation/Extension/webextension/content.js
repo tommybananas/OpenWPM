ccec7d4323749524b319d0d11012822ffb77c8bb
Sai Sunkam <sunk0015@umn.edu>
2016-12-04 14:15:11 -0600

    Added settings to content.js

--- automation/Extension/webextension/content.js
+++ automation/Extension/webextension/content.js
@@ -1,7 +1,23 @@
 console.log('made it!');
 
-
-chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-    console.log(response);
-  console.log(response.farewell);
+chrome.runtime.sendMessage({greeting: "request_settings"}, function(response) {
+    console.log(response.settings);
+    settings = response.settings;
+    go(settings);
 });
+
+function go(settings){
+   if(settings[0]){
+    console.log("Gonna run the 1rd test homie");
+
+    }
+    if(settings[1]){
+        console.log("Gonna run the 2rd test homie");
+
+    }
+    if(settings[2]){
+        console.log("Gonna run the 3rd test homie");
+    }
+
+}
+
